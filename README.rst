3d605050d171e738c60ea78dd47801e8bd28da10
Stephen Newell <sjnewell@gmail.com>
2017-05-25 21:25:43 -0600

    Initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+phantom-pi
+==========
+phantom-pi is a library for accessing and manipulating hardware for a
+Raspberry Pi.  It's designed to be as efficient as possible, ideally providing
+no overhead beyond an efficient C implementation.
---
+++ test/io_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+#include <phantompi/io.hpp>
+
+TEST(Io, write)
+{
+    phantompi::File test{"test.txt"};
+
+    std::string data = "Test";
+    auto ret = test.write(data.c_str(), data.length());
+    ASSERT_EQ(data.length(), ret);
+}
+
+TEST(Io, read)
+{
+    phantompi::File test{"test.txt"};
+
+    char data[5];
+    auto ret = test.read(data, 4);
+    data[ret] = '\0';
+    ASSERT_EQ(4, ret);
+    ASSERT_STREQ("Test", data);
+}
