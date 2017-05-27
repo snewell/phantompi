74ef7d59b0621e0174025621045d1504fa9ea416
Stephen Newell <sjnewell@gmail.com>
2017-05-27 17:01:24 -0600

    Add a test for phantompi::Handle

---
+++ test/handle_test.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include <phantompi/handle.hpp>
+
+TEST(Handle, makeHandle)
+{
+    auto handle = phantompi::makeHandle(10, []() { });
+}
+
+TEST(Handle, cleanupFn)
+{
+    auto closed = false;
+    {
+        auto handle = phantompi::makeHandle(10, [&closed] () {
+            closed = true;
+        });
+    }
+    ASSERT_TRUE(closed);
+}
